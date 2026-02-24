f34ed8942119fa5aad358fedb471655796a064c9
Parveen Yadav <parveeenrao7727@gmail.com>
2026-02-24 09:14:28 +0530

    Remove env

--- app/config.py
+++ app/config.py
@@ -9,10 +9,7 @@ class Setting(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    CLOUDINARY_CLOUD_NAME: str
-    CLOUDINARY_API_KEY: str
-    CLOUDINARY_API_SECRET: str
-
+    
     model_config = SettingsConfigDict(env_file=".env")
 
 
