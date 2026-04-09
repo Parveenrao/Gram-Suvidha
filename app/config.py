be3a7e792a62d337dc89f8dadf1f67cc112922ef
Parveenrao <parveeenrao7727@gmail.com>
2026-04-09 15:49:52 +0530

    fix env

--- app/config.py
+++ app/config.py
@@ -4,13 +4,14 @@ load_dotenv()
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 class Setting(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    
-    model_config = SettingsConfigDict(env_file=".env")
 
+    model_config = SettingsConfigDict()  # ❌ remove env_file
 
 settings = Setting()
